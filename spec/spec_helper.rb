f402a0d57d6d3223e2fff5f1ebd96a8842ec804c
M. Scott Ford <scott@mscottford.com>
2013-03-05 15:03:06 -0500

    Fixes path to shared_behaviors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'webmock/rspec'
 Encoding.default_external = Encoding::UTF_8 if defined?(Encoding)
-Dir.glob(File.join(File.dirname(__FILE__), 'lib', 'shared_behaviors', '**', '.', '*.rb')).each do |f|
+Dir.glob(File.join(File.dirname(__FILE__), 'lib', 'shared_behaviors', '**', '*.rb')).each do |f|
     require f
 end
